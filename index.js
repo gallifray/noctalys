ffe052627f0e4bfc661f035f2e35c4663e623837
Antoine Harel <antoine.harel@epita.fr>
2019-03-26 14:07:59 +0100

    [STRUCTURE] Re-structured whole module

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const path = require('path');
 const fs = require('fs');
 const colors = require('colors')
 
-var express;
-var noct_conf;
+var app;
+var nc;
 
-function mylog() {
-    if (noct_conf.logger) {
+function logger() {
+    if (nc.logger) {
         for (var i = 0; i < arguments.length; i++) {
             process.stdout.write(arguments[i] + " ");
         }
@@ -15,75 +15,99 @@ function mylog() {
 }
 
 var checkParams = () => {
-    if (!fs.existsSync(path.join(noct_conf.server_dir, noct_conf.app_dir))) {
-        console.log(`[noctalys] ❌  Specified application path (${path.join(noct_conf.server_dir, noct_conf.app_dir)}) does not exist. Exiting...`.red.bold)
+    if (!fs.existsSync(path.join(nc.server_dir, nc.app_dir))) {
+        console.log(`[noctalys] ❌  Specified application path (${path.join(nc.server_dir, nc.app_dir)}) does not exist. Exiting...`.red.bold)
         process.exit(1)
     }
 
-    if (!fs.existsSync(path.join(noct_conf.server_dir, noct_conf.app_dir, noct_conf.entry_point))) {
-        console.log(`[noctalys] ❌  Specified application entry point (${path.join(noct_conf.server_dir, noct_conf.app_dir, noct_conf.entry_point)}) does not exist. Exiting...`.red.bold)
+    if (!fs.existsSync(path.join(nc.server_dir, nc.app_dir, nc.entry_point))) {
+        console.log(`[noctalys] ❌  Specified application entry point (${path.join(nc.server_dir, nc.app_dir, nc.entry_point)}) does not exist. Exiting...`.red.bold)
         process.exit(1)
     }
 }
 
 exports.init = (noctalysConf) => {
-    express = noctalysConf.app;
-    noct_conf = noctalysConf;
-    mylog("[noctalys]    Starting...".bold.cyan)
-    mylog("[noctalys] 🛠  Server root path is".cyan, noct_conf.server_dir.yellow)
-    mylog("[noctalys] 📂  Application path is".cyan, path.join(noct_conf.server_dir, noct_conf.app_dir).yellow)
-    mylog("[noctalys] 📄  Application entry point is".cyan, path.join(noct_conf.server_dir, noct_conf.app_dir, noct_conf.entry_point).yellow)
+    app = noctalysConf.app;
+    nc = noctalysConf;
+    if (nc.exclude == null)
+        nc.exclude = []
+
+    filter_excludes(nc.exclude)
+
+    logger("[noctalys]    Starting...".bold.cyan)
+    logger("[noctalys] 🛠  Server root path is".cyan, nc.server_dir.yellow)
+    logger("[noctalys] 📂  Application path is".cyan, path.join(nc.server_dir, nc.app_dir).yellow)
+    logger("[noctalys] 🖥  Application entry point is".cyan, path.join(nc.server_dir, nc.app_dir, nc.entry_point).yellow)
+    if (nc.exclude.length > 0)
+        logger("[noctalys] ⚠️  Excluded path:".cyan)
+    nc.exclude_dirs.concat(nc.exclude_files).forEach(exclude => {
+        logger("[noctalys]      ".cyan, `- ${exclude}`.yellow)
+    });
 
     checkParams();
 
-
-    noct_conf.staticFiles = listFiles(noct_conf.app_dir);
-    noct_conf.staticDirs = listDirs(noct_conf.app_dir);
-
 }
 
-exports.run = () => {
-    noct_conf.staticDirs.forEach((dir) => express.get(`/${dir}/*`, serveStaticDir(`/${noct_conf.app_dir}/${dir}/`)))
-    noct_conf.staticFiles.forEach((file) => express.get(`/${file}`, serveStaticFile(`/${noct_conf.app_dir}/${file}`)))
-    mylog(`[noctalys] ✅  Noctalys is running !`.green.bold)
-}
+exports.listen = () => {
+    logger(`[noctalys] ✅  Noctalys is listening !`.green)
+    app.get('*', (req, res) => {
+        var excluded = isExcluded(path.join(req.params['0']));
 
-exports.defaultRoute = () => {
-    express.get('*', (req, res) => {
-        res.sendFile(path.join(noct_conf.server_dir + `/${noct_conf.app_dir}/${noct_conf.entry_point}`))
-    });
-}
-
-var serveStaticDir = (dir) => {
-    return (req, res) => res.sendFile(path.join(noct_conf.server_dir, dir, req.params['0']), (err) => {
-        if (err)
-            throw err;
+        if (!excluded && fs.existsSync(path.join(nc.server_dir, nc.app_dir, req.params['0']))) {
+            res.sendFile(path.join(nc.server_dir, nc.app_dir, req.params['0']), (err) => {
+                if (err)
+                    res.sendFile(path.join(nc.server_dir + `/${nc.app_dir}/${nc.entry_point}`))
+            })
+        }
+        else
+            res.sendFile(path.join(nc.server_dir + `/${nc.app_dir}/${nc.entry_point}`))
     })
 }
 
-var serveStaticFile = (file) => {
-    return (req, res) => res.sendFile(path.join(noct_conf.server_dir, file), (err) => {
-        if (err)
-            throw err;
+var isExcluded = (route) => {
+    var excluded = false;
+    nc.exclude_dirs.forEach((exclude) => {
+        if (route.startsWith(exclude))
+            excluded = true;
     });
+
+    if (excluded)
+        return true;
+
+    nc.exclude_files.forEach((exclude) => {
+        if (route === exclude)
+            excluded = true;
+    })
+
+    return excluded;
 }
 
-var listDirs = (app_dir) => {
-    var res = []
-    fs.readdirSync(app_dir).forEach(function (file) {
-        if (fs.lstatSync(path.join(app_dir, file)).isDirectory()) {
-            res.push(file)
-        }
-    });
-    return res;
-}
+var filter_excludes = (excludes) => {
+    excludes = excludes.filter((e) => {
+        return !['', '.', ' ', '/'].includes(e);
+    })
 
-var listFiles = (app_dir) => {
-    var res = []
-    fs.readdirSync(app_dir).forEach(function (file) {
-        if (fs.lstatSync(path.join(app_dir, file)).isFile()) {
-            res.push(file)
-        }
-    });
-    return res;
+    nc.exclude_files = excludes.filter((exclude) => {
+        const fullpath = path.join(nc.server_dir, nc.app_dir, exclude);
+        return fs.existsSync(fullpath) && fs.lstatSync(fullpath).isFile();
+    })
+
+    nc.exclude_dirs = excludes.filter((exclude) => {
+        const fullpath = path.join(nc.server_dir, nc.app_dir, exclude);
+        return fs.existsSync(fullpath) && fs.lstatSync(fullpath).isDirectory();
+    })
+
+    nc.exclude_dirs = nc.exclude_dirs.map((exclude) => {
+        if (exclude[0] != '/')
+            exclude = '/' + exclude;
+        if (exclude[exclude.length - 1] != '/')
+            exclude += '/';
+        return exclude;
+    })
+
+    nc.exclude_files = nc.exclude_files.map((exclude) => {
+        if (exclude[0] != '/')
+            exclude = '/' + exclude;
+        return exclude;
+    })
 }
